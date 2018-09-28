edfef562cac1bb026f04533fcdf7db0d8c3abc7e
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-27 19:34:58 -0500

    Defined helper function

--- JS/subsets.js
+++ JS/subsets.js
@@ -4,5 +4,14 @@ console.log("Subsets!");
 // data: an array containing a set.
 // No return value, just prints to std output.
 const subsets = (data) => {
+	// Call helper function passing in given data, empty array that
+	// will hold subsets, and 0 to start at 0th index 
+	// as arguments.
+	helper(data, [], 0);
+}
+
+// Helper Function that will take in data array, subset array, and current index
+// Only prints to std output.
+const helper = (data, subset, i) => {
 	
 }
