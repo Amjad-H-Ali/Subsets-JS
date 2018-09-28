f049d35f8b2af756a0174c0647c22dfba323e30e
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-27 19:30:17 -0500

    Defined function

---
+++ JS/subsets.js
@@ -0,0 +1,8 @@
+console.log("Subsets!");
+
+// Function takes in one parameter:
+// data: an array containing a set.
+// No return value, just prints to std output.
+const subsets = (data) => {
+	
+}
